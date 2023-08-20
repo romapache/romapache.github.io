0e5e81cd32104f665d1ad3a499635e838849690c
romapache <142609147+romapache@users.noreply.github.com>
2023-08-21 01:33:14 +0300

    15

--- main.js
+++ main.js
@@ -96,7 +96,8 @@ function connectDeviceAndCacheCharacteristic(device) {
       then(server => {
         log('GATT server connected, getting service...');
 
-        return server.getPrimaryService(0xFFE0);
+        //return server.getPrimaryService(0xFFE0);
+        return server.getPrimaryService('00001800-0000-1000-8000-00805f9b34fb');
       }).
       then(service => {
         log('Service found, getting characteristic...');
