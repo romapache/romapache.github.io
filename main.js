8b176c2fe4a88c1bbf54bfa81992b01f0ece3e00
romapache <142609147+romapache@users.noreply.github.com>
2023-08-20 16:39:56 +0300

    12

--- main.js
+++ main.js
@@ -53,7 +53,8 @@ function requestBluetoothDevice() {
  // optionalServices: ['00001802-0000-1000-8000-00805f9b34fb','0000180f-0000-1000-8000-00805f9b34fb' , '0000fff0-0000-1000-8000-00805f9b34fb']
     //filters: [/*{services: [0xFFE0]}*/{}],
     filters: [{
-      services: ['Health Thermometer Service'],
+      //services: ['Health Thermometer Service'],
+      name: 'NC150 BT'
     }]
 
    //optionalServices: ['Health Thermometer Service'] // Необходимо для последующего доступа к службе.
